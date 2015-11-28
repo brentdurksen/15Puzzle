60b82a4d7078f332b90224828df2159290947ae4
brentdurksen <brentdurksen@gmail.com>
2015-11-28 13:29:26 -0700

    Updated puzzle logic with (bad) shuffle algorithm

--- js/puzzlelogic.js
+++ js/puzzlelogic.js
@@ -1,6 +1,8 @@
+var puzzleSolvedState = [];
 var puzzleCurrentState = [];
+var puzzleHeight = 4;
 /* Grid coordinates to check against 2D puzzle array */
-/* TODO: Generate map programmatically based on variable grid size */
+/* TODO: Generate map programmatically based on variable puzzleHeight */
 var rowColMap = [[0,0], [0,1], [0,2], [0,3], [1,0], [1,1], [1,2], [1,3],
         [2,0], [2,1], [2,2], [2,3], [3,0], [3,1], [3,2], [3,3]];
 
@@ -9,47 +11,65 @@ $(function() {
 /* Initialize our puzzle */
     initPuzzle();
 
-/* Add some pretty colors */ 
-    $('td').hover(function() {
-        $(this).addClass("active");
-    },
-     function() {
-        $(this).removeClass("active");
-    })
+    /* Shuffle it up!
+    for (i = 0; i < 100; i++) {
+        var clickLoc = Math.floor(Math.random() * (puzzleCurrentState.length));
+        $("#td" + clickLoc).trigger("click");
+        console.log(clickLoc);
+    }    */
     
 /* Process clicks */
     $(".puzzle-piece").click(function() { 
         var clickedLocation = Number(event.target.id.replace('td',''));
-        var clickedNum = puzzleCurrentState[clickedLocation];
         
-        if (clickedNum === ' ') { return; 
+        if (puzzleCurrentState[clickedLocation] === ' ') { return;
         } else {
             if (blankInRow(clickedLocation)) {
-                
+                rearrangeRow(clickedLocation);
             } else if (blankInColumn(clickedLocation)) {
-                
+                rearrangeCol(clickedLocation);
             } 
+            /* TODO: We probably don't need to update the whole page every time! */
+            for (i = 0; i < puzzleCurrentState.length; i ++) {
+                $ ("#td" + i).text(puzzleCurrentState[i]);
+            }
+            /* Did you win!? */
+            if (puzzleCurrentState === puzzleSolvedState) {
+                alert("You win!")
+            }
         }
-        
     });
-        
+
 });
 
 function initPuzzle() {
-    puzzleCurrentState = generateRandomArray();
+    puzzleSolvedState = _.range(1, 16);
+    puzzleSolvedState.push(' ');
+    puzzleCurrentState = puzzleSolvedState;
+    /* This can lead to unsolvable puzzles:
+    puzzleCurrentState = _.shuffle(puzzleSolvedState); */
+    shufflePuzzle();
     $(".puzzle-piece").each(function(index) {
         $(this).text(puzzleCurrentState[index]);
-        if (index + 1 === puzzleCurrentState[index]) {
-            $(this).addClass("success");
-        }
     });
 }
 
-function generateRandomArray() {
-    var puzzleNumbers = _.range(1, 16);
-    puzzleNumbers.push(' ');
-    puzzleNumbers = _.shuffle(puzzleNumbers);
-    return puzzleNumbers;
+function shufflePuzzle() {
+    console.log("I'm shuffling now");
+    var shuffleCount = 0;
+    for (i = 0; i < 50; i++) {
+        var clickedLocation = Math.floor(Math.random() * (puzzleCurrentState.length));
+        if (puzzleCurrentState[clickedLocation] === ' ') {  continue;
+        } else {
+            if (blankInRow(clickedLocation)) {
+                rearrangeRow(clickedLocation);
+            } else if (blankInColumn(clickedLocation)) {
+                rearrangeCol(clickedLocation);
+            }
+        }
+        shuffleCount = shuffleCount+1;
+        }
+    console.log(shuffleCount);
 }
 
 function blankInRow(clickedLoc) {
@@ -62,4 +82,26 @@ function blankInColumn(clickedLoc) {
     var blankSquare = puzzleCurrentState.indexOf(' ');
     if (rowColMap[blankSquare][1] === rowColMap[clickedLoc][1]) { return true; }
     else { return false; }
+}
+
+function rearrangeRow(clickedLocation) {
+    puzzleCurrentState = _.without(puzzleCurrentState, ' ');
+    puzzleCurrentState.splice(clickedLocation, 0, ' ');
+    return;
+}
+
+function rearrangeCol(clickedLocation) {
+    var currColumn = rowColMap[clickedLocation][1];
+    var currRow = rowColMap[clickedLocation][0];
+    /* Generate an array of the column in question */
+    var colCurrentState = []
+    for (i = 0; i < puzzleHeight; i++) {
+        colCurrentState.push(puzzleCurrentState[currColumn+(puzzleHeight*i)]);
+    }
+    colCurrentState = _.without(colCurrentState, ' ');
+    colCurrentState.splice(currRow, 0, ' ');
+    for (i = 0; i < puzzleHeight; i++) {
+        puzzleCurrentState[currColumn+(puzzleHeight*i)] = colCurrentState[i];
+    }
+    return;
 }
